50d1499b337f7b565f7b622f9d274d58a0898371
ArielPetter <arielputzpetter@gmail.com>
2024-04-12 21:55:45 -0300

    update style items interface

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
 //veio assim: import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from "./App";
+//para importação de export default é só import 'nome arquivo', para export é import {nome arquivo}
+import GlobalStyle from "./globalStyle";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-/* veio assim:
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <GlobalStyle />
   </React.StrictMode>
 );
- */
+
+/* no react, não se pode começar com um elemento e terminar com outro, tem que ser sanduiche, papo em cima e pao embaixo, por isso a div mas como é apenas para isso, foi invetado o fragment que é <> </> */
--- src/style.js
+++ src/style.js
@@ -1,32 +1,121 @@
-import styled from 'styled-components'
-import background from './assets/background1.svg'
+import styled from "styled-components";
+import background from "./assets/background1.svg";
 
+export const Container = styled.div`
+  background: url("${background}");
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 40px;
+  height: 100vh;
+`;
 
-export const Container = styled.div `
-    background: url("${background}");
-    background-size: cover;
-`
+export const Image = styled.img`
+  margin-top: 40px;
+`;
 
-export const Image = styled.img `
+export const ContainerItems = styled.div`
+  background: linear-gradient(
+    157.44deg,
+    rgba(255, 255, 255, 0.6) 0.84%,
+    rgba(255, 255, 255, 0.6) 0.85%,
+    rgba(255, 255, 255, 0.15) 100%
+  );
+  border-radius: 61px 61px 0px 0px;
+  padding: 50px 36px;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
 
-`
+export const H1 = styled.h1`
+  color: white;
+  text-align: center;
+  margin-top: 10px;
+  margin-bottom: 60px;
+  font-size: 28px;
+`;
 
-export const ContainerItems = styled.div `
+export const InputLabel = styled.p`
+  color: white;
+  padding-left: 25px;
+  width: 47px;
+  height: 22px;
+  line-height: 22px;
+  font-size: 20px;
+  margin-bottom: 3px;
+  font-weight: normal;
+`;
 
-`
+export const Input = styled.input`
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  background: rgba(255, 255, 255, 0.25);
+  outline: none;
+  border: none;
+  border-radius: 14px;
+  width: 342px;
+  height: 58px;
+  padding-left: 25px;
+  margin-bottom: 10px;
+  color: white;
+  //escrita no input:
+  font-size: 20px;
+  line-height: 28px;
+  font-weight: normal;
+`;
 
-export const H1 = styled.h1 `
+export const Button = styled.button`
+  color: white;
+  background: black;
+  width: 342px;
+  height: 74px;
+  border-radius: 14px;
+  border: none;
+  outline: none;
+  margin-top: 40px;
+  cursor: pointer;
+  //posição da seta
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 15px;
+  //escrita do botão:
+  font-size: 18px;
 
-`
+  &:hover {
+    opacity: .9;
+  }
 
-export const InputLabel = styled.p `
+  &:active {
+    opacity: .8;
+  }
+`;
 
-`
+export const UserList = styled.li`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  background: rgba(255, 255, 255, 0.25);
+  border-radius: 14px;
+  outline: none;
+  border: none;
+  width: 342px;
+  height: 58px;
+  margin: 20px 0;
 
-export const Input = styled.input `
+    p {
+      color: white;
+      font-size: 20px;
+      line-height: 28px;
+      font-style: normal;
+      font-weight: normal;
+    }
 
-`
-
-export const Button = styled.button `
-
-`
+    button {
+      background: none;
+      border: none;
+      cursor: pointer;
+    }
+`;
